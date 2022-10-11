aad49a6f4a1e3998b07ff217a024402d1e4f3725
jayashri23 <jsjankar23@gmail.com>
2022-10-11 19:20:01 +0530

    UsingSwitchConditionEmployeeWage

--- src/TillConditionReaches.java
+++ src/TillConditionReaches.java
@@ -1,5 +1,3 @@
-package EmployeeWage;
-
 import java.util.Random;
 
 
---
+++ src/UsingSwitchCase.java
@@ -0,0 +1,30 @@
+package EmployeeWage;
+
+import java.util.Random;
+
+
+
+
+//solve usingswitch case
+public class UsingSwitchCase {
+    public static void main(String [] args) {
+        int salary = 0, fullhr = 16, parthr = 8, wage = 20;
+        Random rand = new Random();
+        int x = rand.nextInt(3);
+        switch(x)
+        {
+            case 2:
+                System.out.println("Employee full time present");
+                salary = (fullhr * wage);
+                System.out.println("" +salary);
+                break;
+            case 1:
+                System.out.println("Employee part time present");
+                salary = (parthr * wage);
+                System.out.println("" +salary);
+                break;
+            default:
+                System.out.println("Employee is absent");
+        }
+    }
+}
