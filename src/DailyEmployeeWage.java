e742b35e45c64d084afd884b8837c16d6d73445b
jayashri23 <jsjankar23@gmail.com>
2022-10-11 19:07:50 +0530

    DailyeEmployeeWage

---
+++ src/DailyEmployeeWage.java
@@ -0,0 +1,21 @@
+package EmployeeWage;
+
+
+
+
+//calculate Daily employee wage
+import java.util.Random;
+
+public class DailyEmployeeWage {
+    public static void main(String [] args) {
+        int hr = 8, wage = 20, salary = 0;
+        Random rand = new Random();
+        int x = rand.nextInt(2);
+        if (x == 1) {
+            System.out.println("Employee is present");
+            salary = (hr * wage);
+            System.out.println("" + salary);
+        } else
+            System.out.println("Employee is absent");
+    }
+}
